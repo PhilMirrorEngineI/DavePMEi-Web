b72e71753be731e48c98f7b5bf8c6fc38a583d8e
Phil <phildos@hotmail.co.uk>
2025-10-02 16:25:03 +0100

    Update donatesection.tsx

--- app/components/donatesection.tsx
+++ app/components/donatesection.tsx
@@ -2,7 +2,7 @@
 
 import { button } from "./ui/Button";
 
-export function BonateSection() {
+export function ButtonSection() {
   return (
     <div className="text-center mt-6">
       <Button onClick={() => alert("Redirect to donations page")}>Donate £5</Button>
