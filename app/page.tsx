a40052784458b04a314547c045363279589d2990
Phil <phildos@hotmail.co.uk>
2025-10-02 18:03:56 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -204,20 +204,3 @@ export default function HomePage() {
     </div>
   );
 }
-"use client";
-
-import React from "react";
-
-export default function Button({
-  children,
-  ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      className="px-4 py-2 rounded bg-yellow-500 hover:bg-yellow-600 text-black font-bold"
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
