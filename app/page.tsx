6a5db557b6a3c9bf5d4ada386e41c8d92ea61c50
Phil <phildos@hotmail.co.uk>
2025-10-02 16:27:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Card } from "./components/ui/card";
-import DaveChat from "./components/dave-chat";
+import DaveChat from "./components/davechat";
 import DonateSection from "./components/donatesection";
 
 export default function HomePage() {
