34926ac07ff69b9472ca39f5f7d1c67950ca5aec
Phil <phildos@hotmail.co.uk>
2025-10-02 15:28:22 +0100

    Update and rename Dave-chat.tsx to dave-chat.tsx

--- app/components/dave-chat.tsx
+++ app/components/dave-chat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export function DaveChat() {
+export function dave-chat () {
   return (
     <div className="fixed bottom-4 right-4 p-4 border border-yellow-400/40 bg-black/70 text-white rounded-lg">
       Dave Chat Widget (placeholder)
