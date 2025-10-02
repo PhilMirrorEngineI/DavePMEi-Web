2abd604cc6b400c521cff4f6c58e40ca10e1581f
Phil <phildos@hotmail.co.uk>
2025-10-02 16:09:42 +0100

    Update dave-chat.tsx

--- app/components/dave-chat.tsx
+++ app/components/dave-chat.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-export function dave-chat () {
-  return (
-    <div className="fixed bottom-4 right-4 p-4 border border-yellow-400/40 bg-black/70 text-white rounded-lg">
-      Dave Chat Widget (placeholder)
-    </div>
-  );
+export default function DaveChat() {
+  return <div>DaveChat Widget</div>;
 }
