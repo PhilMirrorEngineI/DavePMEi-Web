e3e1d4d2c594b5b26a35736488375c9339f74793
Phil <phildos@hotmail.co.uk>
2025-10-02 16:12:44 +0100

    Update card.tsx

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,5 +1,11 @@
 "use client";
 
-export default function DaveChat() {
-  return <div>DaveChat Widget</div>;
+import * as React from "react";
+
+export default function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="rounded-lg border border-yellow-400/40 bg-black/70 text-white p-4 shadow-lg">
+      {children}
+    </div>
+  );
 }
